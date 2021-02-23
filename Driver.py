c97ad9e2efd8d7c788d1118445dfc68cba9ec55f
Vairon M <vairon@live.com>
2021-02-23 12:35:45 -0500

    Added unit tests

--- Driver.py
+++ Driver.py
@@ -1,1 +1,12 @@
-print("Hello World")
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+    def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+
+    def test_strip(self):
+        self.assertEqual("     foo     ".strip(), "foo")
+
+
+if __name__ == '__main__':
+    unittest.main()
