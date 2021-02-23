b1e4f84cf30309567732f4842b0aff096d802932
Vairon M <vairon@live.com>
2021-02-23 13:21:32 -0500

    Made unit tests specific to flask framework, rather than generic ones.

--- Test.py
+++ Test.py
@@ -1,11 +1,14 @@
 import unittest
-
-class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-
-    def test_strip(self):
-        self.assertEqual("     foo     ".strip(), "foo")
+from driver import app
+class TestSimpleFlaskApp(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
+    
+    def test_hello(self):
+        rv = self.app.get('/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World!\n')
 
 if __name__ == '__main__':
     unittest.main()
