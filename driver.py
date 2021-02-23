dc386b2bebdfb8e6bc3aebb9e35404f4e151e513
Vairon M <vairon@live.com>
2021-02-23 17:12:27 -0500

    Hello route now requires name to be added

--- driver.py
+++ driver.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -6,9 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/hello')
-def hello():
-    return '<h1>Hello World!</h1>\n'
+@app.route('/hello/<firstname>')
+def hello(firstname):
+    return '<h1>Hello %s</h1>\n' % escape(firstname)
 
 if __name__ == '__main__':
     app.run()
--- test.py
+++ test.py
@@ -11,6 +11,13 @@ class TestSimpleFlaskApp(unittest.TestCase):
         self.assertEqual(rv.status, '200 OK')
         self.assertEqual(rv.data, b'<h1>Hello World!</h1>\n')
 
+    def test_hello_with_name(self):
+        name = "John"
+        rv = self.app.get('/John')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'<h1>Hello John!</h1>\n')
+        
+
 if __name__ == '__main__':
     import xmlrunner
     runner = xmlrunner.XMLTestRunner(output='test-reports')
