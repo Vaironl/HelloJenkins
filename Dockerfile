c424b5104436a0f5f34127ccacb8f6fea2f438fd
Vairon M <vairon@live.com>
2021-02-24 17:07:39 -0500

    Changing files to test app

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 # This Dockerfile is meant to be run by Jenkins
 FROM python:3.9.2
 
-WORKDIR /usr/src/tests
+WORKDIR /usr/src/
 
-COPY ../requirements.txt ./
+COPY ./requirements.txt ./
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY ./ /usr/src/tests
+COPY ./ /usr/src/
+
+WORKDIR /usr/src/tests/
+
+CMD [ "python", "test.py" ]
--- tests/test.py
+++ tests/test.py
@@ -1,19 +1,16 @@
-import unittest
-from app.driver import app
+import pytest
+import flask
 
-class TestSimpleFlaskApp(unittest.TestCase):
+class test_simple_app(app):
     def setUp(self):
         app.testing = True
         self.app = app.test_client()
     
-    def test_hello_with_name(self):
-        name = "John"
-        rv = self.app.get('/hello/John')
-        self.assertEqual(rv.status, '200 OK')
-        self.assertEqual(rv.data, b'<h1>Hello John!</h1>\n')
+#     def test_hello_with_name(self):
+#         name = "John"
+#         rv = self.app.get('/hello/John')
+#         self.assertEqual(rv.status, '200 OK')
+#         self.assertEqual(rv.data, b'<h1>Hello John!</h1>\n')
         
 
-if __name__ == '__main__':
-    import xmlrunner
-    runner = xmlrunner.XMLTestRunner(output='test-reports')
-    unittest.main(testRunner=runner)
+# if __name__ == '__main__':
